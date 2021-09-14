be8570670b3e2626deb1b1b3d84ac68dac210ca2
tomhine792 <tomhine792@outlook.com>
2021-09-14 07:42:07 +0100

    use window size hook to change image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import Head from 'next/head';
 import InfoBar from '../components/InfoBar';
 import HeroImage from '../components/HeroImage';
-import Main from '../components/wrappers/Main';
+import { Main } from '../components/Elements';
+import useWindowSize from '../hooks/useWindowSize';
 
 export default function Home() {
+  const winWidth = useWindowSize().width;
+  const heroImgSrc =
+    winWidth <= 425
+      ? 'hero-img-mobile-425.jpg'
+      : 'woman-skeletal-hand-top-pink-bg.jpg';
+  const heroImgWidth = winWidth <= 425 ? 425 : 2400;
+  const heroImgHeight = winWidth <= 425 ? 637 : 1600;
+
   return (
     <div className="">
       <Head>
@@ -12,15 +21,17 @@ export default function Home() {
       </Head>
       <InfoBar />
       <HeroImage
-        src="/hero-img-mobile.jpg"
-        width="3933"
-        height="5900"
+        src={`/${heroImgSrc}`}
+        width={heroImgWidth}
+        height={heroImgHeight}
         alt="a dressed human"
         link="#"
         text="do this thing"
       />
       <Main>
-        <h2>Popular Categories</h2>
+        <h2 className="text-xl font-medium text-gray-800">
+          Popular Categories
+        </h2>
       </Main>
     </div>
   );
